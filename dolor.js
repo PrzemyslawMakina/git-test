0bc37896aa8c7b87242776f20ce6b8734b3e32ee
Przemyslaw Makina <przemyslawmakina@gmail.com>
2024-01-21 08:50:32 +0100

    Another console.log

--- dolor.js
+++ dolor.js
@@ -1,8 +1,8 @@
-const VALUE_1 = 10;
-const VALUE_2 = 29;
+const VALUE_1 = 10
+const VALUE_2 = 29
 
 console.log('Dolor sit amet!')
-const result = VALUE_1 + VALUE_2;
-console.log('Calculating');
-console.log(`Result is ${result}`);
-console.log('Dolor sit amet!');
+const result = VALUE_1 + VALUE_2
+console.log('Calculating')
+console.log(`Result is ${result}`)
+console.log('Dolor sit amet!')
